6517522309cc72268219c823ee9fafe7ddcedd5d
Jérémy Trindade <jeremytrindade@gmail.com>
2020-04-23 20:36:54 +0200

    1º Update

---
+++ projetoTeste/venvProjTest/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jeremydatrindade/Desktop/django/projetoTeste/venvProjTest/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
