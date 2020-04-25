df943dd133747c66e733171c535a8bc0684397fb
Jérémy Trindade <jeremytrindade@gmail.com>
2020-04-25 14:36:38 +0200

    19. CRUD - Update ( Atualizando cadastros existentes )

--- projetoTeste/meuPrimeiroProjeto/clientes/urls.py
+++ projetoTeste/meuPrimeiroProjeto/clientes/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import persons_list
+from .views import persons_new
+from .views import persons_update
 
 
 urlpatterns = [
-    path('list/', persons_list),
+    path('list/', persons_list, name="person_list"),
+    path('new/', persons_new, name="person_new"),
+    path('update/<int:id>/', persons_update, name="person_update"),
 ]
